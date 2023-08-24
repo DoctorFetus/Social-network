359786cdb6028bbd08b6cb2cbb0a1b40cc16146b
Mikhail Palkin <thefireofcoall@gmail.com>
2023-08-25 00:27:02 +0300

    thunk test

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,73 +1,85 @@
-import axios from "axios";
-import {FormDataType} from "../components/Login/LoginForm/LoginForm";
-import {UserProfileType} from "../redux/redusers/profile-reducer";
+    import axios from "axios";
+    import {FormDataType} from "../components/Login/LoginForm/LoginForm";
+    import {UserProfileType} from "../redux/redusers/profile-reducer";
 
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    withCredentials: true,
-    headers: {
-        "API-KEY": "2db0e584-6ebd-4d77-859c-a25eed5599fb"
-    }
-})
+    const instance = axios.create({
+        baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+        withCredentials: true,
+        headers: {
+            "API-KEY": "2db0e584-6ebd-4d77-859c-a25eed5599fb"
+        }
+    })
 
-export const usersAPI = {
-    getUsers(currentPage: number = 0, pageSize: number = 4) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-            .then(response => response.data)
-    },
-    unfollowUser(userID: number) {
-        return instance.delete(`follow/${userID}`)
-            .then(response => response.data)
-    },
-    followUser(userID: number) {
-        return instance.post(`follow/${userID}`)
-            .then(response => response.data)
-    }
-}
-
-export const profileApi = {
-    getProfile(userID: string) {
-        return instance.get(`profile/${userID}`)
-    },
-    getStatus(userID: string) {
-        return instance.get(`profile/status/${userID}`)
-    },
-    updateStatus(status: string) {
-        return instance.put('profile/status', {status})
-    },
-    updatePhoto(newPhoto: File){
-        const formData = new FormData();
-        formData.append('image', newPhoto)
-        return instance.put('profile/photo', formData,  {
-           headers: {
-               'Content-type': 'multipart/form-data'
-           }
-        })
-    },
-    updateProfileData(newProfileData: Partial<UserProfileType>) {
-        return instance.put('profile', newProfileData)
-    }
-}
-
-export const authApi = {
-    authMe() {
-        return instance.get(`auth/me`)
-            .then(response => response.data)
-    },
-    loginIn(FormData: FormDataType) {
-      return instance.post('auth/login', FormData)
-          .then(response => response.data)
-    },
-    logout() {
-        return instance.delete('auth/login')
-            .then(response => response.data)
+    export const usersAPI = {
+        getUsers(currentPage: number = 0, pageSize: number = 4) {
+            return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+                .then(response => response.data)
+        },
+        unfollowUser(userID: number) {
+            return instance.delete(`follow/${userID}`)
+                .then(response => response.data)
+        },
+        followUser(userID: number) {
+            return instance.post(`follow/${userID}`)
+                .then(response => response.data)
+        }
     }
 
-}
-
-export const securityApi = {
-    getCaptcha() {
-        return instance.get('security/get-captcha-url')
-            .then(response => response.data)
+    export const profileApi = {
+        getProfile(userID: string) {
+            return instance.get(`profile/${userID}`)
+        },
+        getStatus(userID: string) {
+            return instance.get(`profile/status/${userID}`)
+        },
+        updateStatus(status: string) {
+            return instance.put('profile/status', {status})
+        },
+        updatePhoto(newPhoto: File){
+            const formData = new FormData();
+            formData.append('image', newPhoto)
+            return instance.put('profile/photo', formData,  {
+               headers: {
+                   'Content-type': 'multipart/form-data'
+               }
+            })
+        },
+        updateProfileData(newProfileData: Partial<UserProfileType>) {
+            return instance.put('profile', newProfileData)
+        }
     }
-}
+
+    export const authApi = {
+        authMe() {
+            return instance.get(`auth/me`)
+                .then(response => response.data)
+        },
+        loginIn(FormData: FormDataType) {
+          return instance.post('auth/login', FormData)
+              .then(response => response.data)
+        },
+        logout() {
+            return instance.delete('auth/login')
+                .then(response => response.data)
+        }
+
+    }
+
+    export const securityApi = {
+        getCaptcha() {
+            return instance.get('security/get-captcha-url')
+                .then(response => response.data)
+        }
+    }
+
+    export enum ResultCodes {
+        OK = 0,
+        ERROR = 1,
+        CAPTCHA = 10
+    }
+
+    export type FollowUserResponseType = {
+        resultCode: ResultCodes
+        messages: string[]
+        data: {}
+    }
---
+++ src/tests/user-reducer.thunk.test.ts
@@ -0,0 +1,24 @@
+ import {ResultCodes, usersAPI} from "../api/api";
+    import {acceptFollowUser, followUnfollowUser, toggleIsFollowing} from "../redux/redusers/users-reducer";
+    jest.mock("../api/api");
+
+    const userAPIMock = usersAPI as jest.Mocked<typeof usersAPI>
+
+    test('follow user thunk', async () => {
+        const mockFollowUser = jest.fn().mockResolvedValue({
+            resultCode: ResultCodes.OK,
+            messages: [],
+            data: {}
+        });
+
+        userAPIMock.followUser = mockFollowUser;
+
+        const thunk = acceptFollowUser(1);
+        const dispatch = jest.fn();
+        await thunk(dispatch);
+        expect(dispatch).toBeCalledTimes(3);
+        expect(dispatch).toHaveBeenNthCalledWith(1, toggleIsFollowing(1, true))
+        expect(dispatch).toHaveBeenNthCalledWith(2, followUnfollowUser(1, true))
+        expect(dispatch).toHaveBeenNthCalledWith(3, toggleIsFollowing(1, false))
+
+    });
